1fd5b9ad6dda343880abf7439ac6f20ae84e7833
VanthuirMaia <vanmaiasf@gmail.com>
2025-08-21 20:38:45 -0300

    feat - Atualizado kpis.sql - 2. Atraso Médio

--- sql/kpis.sql
+++ sql/kpis.sql
@@ -5,7 +5,7 @@
 -- Data: 21-08-2025
 -- ===========================================
 
---1. OTD (On-Time Delivery %)
+-- 1. OTD (On-Time Delivery %)
 SELECT
     DATE_TRUNC('month', order_date) AS mes,
     COUNT(*) AS total_entregas,
@@ -15,3 +15,26 @@ FROM newfase.stg_dados
 WHERE order_date IS NOT NULL AND trim(order_date) <> ''
 GROUP BY mes
 ORDER BY mes;
+
+-- 2. Atraso Médio
+SELECT
+    DATA_TRUNC('month', order_date) AS mes,
+    ROUND(AVG(delay_days), 2) AS atraso_medio
+FROM newfase.stg_dados
+WHERE orders_date IS NOT NULL AND trim(order_date) <> ''
+GROUP BY mes
+ORDER BY mes;
+
+-- 3. Tempo Médio de Entrega
+SELECT 
+  DATE_TRUNC('month', order_date::date) AS mes,
+  ROUND(AVG(delivery_date::date - ship_date::date), 2) AS tempo_medio_entrega
+FROM newfase.stg_dados
+WHERE delivery_date IS NOT NULL 
+  AND ship_date IS NOT NULL
+  AND trim(delivery_date) <> ''
+  AND trim(ship_date) <> ''
+  AND trim(order_date) <> ''
+GROUP BY mes
+ORDER BY mes;
+
